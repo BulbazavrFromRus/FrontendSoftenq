535db8a3e1c7b45d9550939cae81ad228720a72f
Danila <dbulbakov@bk.ru>
2023-11-20 10:34:27 +0600

    Nothing change, can't display messages

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,77 +1,52 @@
-import React, {useEffect,useRef, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import {useLocation, useNavigate} from 'react-router-dom';
-import io from 'socket.io-client'
-import data from "jsonwebtoken/lib/JsonWebTokenError.js";
-//import {useEffect} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import io from 'socket.io-client';
 
-const socket =io.connect("http://localhost:8001")
-
+const socket = io.connect("http://localhost:8001");
 
 function UserForm() {
     const navigate = useNavigate();
     const location = useLocation();
 
-
     const token = localStorage.getItem('token');
+    const [message, setMessage] = useState('');
+    const [messageReceived, setMessageReceived] = useState('');
 
-    const[message, setMessage] = useState([])
-    const[messageReceived, setMessageReceived] = useState([])
-
-    //we find 'username' in localStorage
-    //state - состояние
-    const {username} = location.state;
+    const { username } = location.state;
 
 
-    if(token)
-    {
-        //Axios.defaults.headers.common['Authorization'] = 'Bearer ${token}';
+    if (token) {
         Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-    }
-    else
-    {
-        navigate("/", {replace: true});
-        console.log("No token provided!")
+    } else {
+        navigate("/", { replace: true });
+        console.log("No token provided!");
     }
 
 
-    //we create function after that we will send message
-    //by putting on the button we will send message
-    const sendMessage = ()=>{
-        socket.emit("send_message", message)
+    const sendMessage = () => {
+        socket.emit("send_message", message);
+        console.log('Sent message:', message);
     };
 
     useEffect(() => {
         socket.on("receive_message", (data) => {
-             setMessageReceived(data.message);
-         });
-/*
-
-       // console.log("fgesdfsds");
-    socket.on("connection", () => {
-            if(socket.connected){
-                console.log("Everything is good")
-            }
-            socket.emit("send_message", "AASAAAAAAAA")
-            console.log("conioecewfw");*/
-       /* });*/
-
-    }, [socket]);
-
-
+            console.log(data.message)
+            setMessageReceived(data.message);
+        });
+    }, []);
 
     return (
         <div>
             <h2>{username} successfully signed in </h2>
-            <div className = "UserForm">
-                <input placeholder="Message..." onChange= {(event)=>
-                setMessage(event.target.value)
-                }/>
+            <div className="UserForm">
+                <input
+                    placeholder="Message..."
+                    onChange={(event) => setMessage(event.target.value)}
+                />
                 <button onClick={sendMessage}>Send message</button>
                 <h1>Message:</h1>
-                <h1>{messageReceived}</h1>
-
+                <div className="messages">{messageReceived}</div>
             </div>
         </div>
     );
