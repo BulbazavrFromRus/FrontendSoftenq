de01c7ff01c39c2fc95863edcb6a3377137eaac3
Danila <dbulbakov@bk.ru>
2023-11-30 11:14:55 +0600

    I added border for messages and messages display as a list

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
@@ -46,7 +46,70 @@ function UserForm() {
                 />
                 <button onClick={sendMessage}>Send message</button>
                 <h1>Message:</h1>
-                <div className="messages">{messageReceived}</div>
+                <div className="messages">
+                    {messageReceived}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserForm;*/
+
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:8001");
+
+function UserForm() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const token = localStorage.getItem('token');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]); // New state for messages
+    const { username } = location.state;
+
+    if (token) {
+        Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        navigate("/", { replace: true });
+        console.log("No token provided!");
+    }
+
+    const sendMessage = () => {
+        socket.emit("send_message", message);
+        console.log('Sent message:', message);
+        setMessage(''); // Clear the input field after sending the message
+    };
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            console.log(data);
+            setMessages(prevMessages => [...prevMessages, data]);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h2>{username} successfully signed in </h2>
+            <div className="UserForm">
+                <input
+                    placeholder="Message..."
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                />
+                <button onClick={sendMessage}>Send message</button>
+                <h1>Messages:</h1>
+                <div className="messages">
+                    <ul>
+                            {messages.map((msg, index) => (
+                                <li key={index}>{msg}</li>
+                            ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
